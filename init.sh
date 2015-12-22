f80236687733c4b11ac5077b564cf345fce2659a
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-12-23 01:11:36 +0900

    fix

--- init.sh
+++ init.sh
@@ -7,11 +7,9 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew doctor
-
 brew update
 
 brew install python
-
 brew install ansible
 
 brew install git
